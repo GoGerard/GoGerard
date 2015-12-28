481cd704525ae8d8412fd41d19a29699c7f99e94
Rivalo <NoSpam@plox.com>
2015-12-28 19:12:47 +0000

    Added .sh file for linux

---
+++ GitGoGerard.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Retrieve GoGerard repos
+
+git clone https://github.com/GoGerard/GerardDiscord.git
+git clone https://github.com/GoGerard/GerardAPI.git
+git clone https://github.com/GoGerard/GerardJS.git
+echo "NOTE: add conf.json to GerardDiscord"
+echo "NOTE: Specify API IP as seen by client in GerardJS/assets/js/app.js"
+echo "Run 'docker-compose up' in terminal that is connected to docker machine"
